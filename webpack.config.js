bbbf26e87690a5da8f9f091497ca6fb9c83fc36d
SanFili <filimonova.alexandra@yandex.ru>
2020-05-26 10:10:25 +0300

    настроена обработка css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -11,7 +12,14 @@ module.exports = {
             test: /\.js$/, // регулярное выражение, которое ищет все js файлы
             use: { loader: "babel-loader" }, // весь JS обрабатывается пакетом babel-loader
             exclude: /node_modules/ // исключает папку node_modules
+            },
+            {
+            test: /\.css$/, // применять это правило только к CSS-файлам
+            use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
             }
         ]
-    }
+    },
+    plugins: [ 
+        new MiniCssExtractPlugin({filename: 'style.css'})
+    ]
 }
