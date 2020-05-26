f554388b474351f7c1a2d9ce367396e323699fc2
SanFili <filimonova.alexandra@yandex.ru>
2020-05-26 10:20:54 +0300

    установлен postCSS

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
             },
             {
             test: /\.css$/, // применять это правило только к CSS-файлам
-            use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
             }
         ]
     },
@@ -27,6 +27,7 @@ module.exports = {
             inject: false, // стили НЕ нужно прописывать внутри тегов
             template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
             filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-          })
+        }),
+        new WebpackMd5Hash()
     ]
 }
